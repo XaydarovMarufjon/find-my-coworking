cd228f33fbc83ccf9fab04a62ca90af4faaa2013
XaydarovMarufjon <marufjankhaydarov@gmail.com>
2024-01-12 16:44:27 +0500

    chore: :sparkles: Add error page

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from "react-router-dom"
 import Index from "./pages"
 import Coworking from "./pages/coworking"
+import ErrorPage from "./error-page"
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Index />,
+    errorElement: <ErrorPage />
   },
   {
     path: "/coworking/:id",
