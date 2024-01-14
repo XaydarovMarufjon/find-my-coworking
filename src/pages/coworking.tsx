8c2514c8a120335494fda77ecb43f706108f810a
XaydarovMarufjon <marufjankhaydarov@gmail.com>
2024-01-14 18:14:36 +0500

    publish branch

--- src/pages/coworking.tsx
+++ src/pages/coworking.tsx
@@ -3,5 +3,5 @@ import { useParams } from "react-router-dom";
 export default function Coworking() {
   const { id } = useParams();
 
-  return <div>You're viewing coworking #{id}</div>
+  return <div>You're viewing coworking  your id  #{id}</div>
 }
